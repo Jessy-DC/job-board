4ddd7f0ceddbc8a40b2bf71f8398fc31c9ccc6d3
Jessy CHARLOT <50245217+Jessy-DC@users.noreply.github.com>
2023-03-10 00:17:09 +0100

    fix index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
-import type { NextPage } from 'next';
+import type {GetServerSideProps, NextPage} from 'next';
 import {JobList} from "@/components/JobList";
 import {Layout} from "@/components/Layout";
+import {
+    deserializeJobSummary,
+    SerializedJobSummary,
+    serializeJobSummary,
+} from "@/lib/jobs";
+import {getJobs} from "@/lib/jobs_server";
 
-<<<<<<< HEAD
 const Home: NextPage<Props> = ({initialJobs: serializedInitialJobs}) => {
     const initialJobs = serializedInitialJobs.map(deserializeJobSummary);
     return (
@@ -17,20 +22,12 @@ export interface Props {
 }
 
 export const getServerSideProps: GetServerSideProps<Props> = async () => {
-    const initialJobs = await getJobs({});
+    const initialJobs = await getJobs({page: 1});
     return {
         props: {
             initialJobs: initialJobs.map(serializeJobSummary)
         }
     }
-=======
->>>>>>> parent of c4bd79d (Filled jobs with GetServerSideProps)
-const Home: NextPage = () => {
-  return (
-    <Layout>
-        <JobList />
-    </Layout>
-  )
 }
 
 export default Home;
