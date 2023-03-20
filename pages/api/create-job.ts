f4a4585d72ad39bbf7807007cc588eb649db3757
Jessy CHARLOT <50245217+Jessy-DC@users.noreply.github.com>
2023-03-20 23:36:01 +0100

    Create job with form

--- pages/api/create-job.ts
+++ pages/api/create-job.ts
@@ -1,5 +1,7 @@
 import {NextApiHandler} from "next";
 import {getToken} from "next-auth/jwt";
+import {JobFormValues, validateJobFormValues} from "@/lib/jobForm";
+import prisma from "@/lib/prisma_server";
 
 const handle: NextApiHandler = async (req, res) => {
     if(req.method !== "POST") {
@@ -12,6 +14,25 @@ const handle: NextApiHandler = async (req, res) => {
         res.status(401).send({ok: false})
         return;
     }
+
+    const values: JobFormValues = JSON.parse(req.body);
+
+    const errors = validateJobFormValues(values);
+    if (Object.keys(errors).length > 0) {
+        res.status(422).send(errors);
+        return;
+    }
+
+    const job = await prisma.job.create({
+        data: {
+            jobTitle: values.jobTitle,
+            company: values.company,
+            description: values.description,
+            applyUrl: values.applyUrl
+        },
+    });
+
+    res.send({jobId: job.id});
 
     res.send({ok: true});
 }
